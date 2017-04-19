2bec8cbb683bbfd019dab7ae265f5607893514c6
sunbaobao <843677724@qq.com>
2017-04-19 09:31:49 +0800

    合并Express

---
+++ app.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const flash = require('connect-flash');
+const routes = require('./routes/index');
+const im=require("./routes/im");
+const settings = require('./settings');
+const users = require('./routes/users');
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(flash());
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+    secret: settings.cookieSecret,
+    key: settings.db,//cookie name
+    cookie: {maxAge: 1000 * 60 * 60 * 24 * 30},//30 days
+    store: new MongoStore({
+        /* db: settings.db,
+         host: settings.host,
+         port: settings.port,*/
+        url: 'mongodb://localhost/blog'
+    }),
+    resave: false,
+    saveUninitialized: true
+}));
+app.use('/', routes);
+app.use('/users', users);
+app.use('/im', im);
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+
+module.exports = app;
---
+++ index.js
@@ -0,0 +1,45 @@
+var express=require("express");
+
+var app = express();
+var path=require("path");
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+app.use(express.static(path.join(__dirname,"public")));
+
+/*
+app.get('/', function(req, res){
+    res.sendfile('./public/index.html');
+});
+*/
+var gustNumber=0;
+var nickNames={};
+var rooms=[];
+var currentRoom={};
+io.on('connection', function(socket){
+    socket.on('new message', function(msg){
+        io.emit('chat message', msg)
+    });
+    socket.on("add user",function (user) {
+
+       if(user){
+           gustNumber++;
+           nickNames[socket.id]=user;
+           var logM="欢迎"+user+"加入聊天室(∩_∩)";
+           io.emit("log",logM);
+           //console.log(gustNumber);
+       }
+    });
+    socket.on('disconnect', function(){
+        if(typeof nickNames[socket.id]!=="undefined"){
+            var user= nickNames[socket.id];
+            delete nickNames[socket.id];
+            gustNumber--;
+            var logM=user+"退出了聊天室(づ｡◕‿‿◕｡)づ！";
+            io.emit("log",logM);
+            // console.log(gustNumber);
+        }
+    });
+});
+http.listen(4010, function(){
+    console.log('listening on *:4010');
+});
