f08066b1a6f512223ece265d32c6fc762fd0c85c
sunbaobao <843677724@qq.com>
2020-08-24 09:43:54 +0800

    loginit

--- src/controller/log/log.js
+++ src/controller/log/log.js
@@ -10,8 +10,8 @@ class LogCTL {
    * @next
    * **/
   insertLog(req, res, next) {
-    let { username, time, browser, description } = req.body
-    Log.create({ username, IP:req.ip, browser, description }).then(function(doc) {
+    let { username, browser, description } = req.body
+    Log.create({ username, IP: req.ip, browser, description }).then(function (doc) {
       if (doc) {
         res.tools.setJson(0, '插入成功')
       } else {
@@ -23,16 +23,22 @@ class LogCTL {
   /**查询日志
    * **/
   searchLog(req, res, next) {
-    let { username = '', time = [] } = req.body
-    let userArr = username.replace(',', '|')
-    console.log('用户名，时间', username, time, userArr, req.ip)
+    let { username = '', time = [], currentPage = 1, pageSize = 10 } = req.body;
+    let userArr = username.replace(',', '|');
+    // console.log('用户名，时间', username, time, userArr, req.ip);
+    if (time.length === 0) {
+      return res.tools.setJson(1, '时间不能为空');
+    }
     Log.find({
       username: new RegExp(userArr),
       time: { $gt: time[0], $lt: time[1] }
-    }).then(function(doc) {
+    }).then((doc) => {
       console.log('查询数据库结果', doc)
-      res.tools.setJson(0, '查询成功', doc)
-    })
+      res.tools.setJson(0, '查询成功', {
+        logData: doc.slice((currentPage - 1) * pageSize, currentPage * pageSize),
+        total: doc.length
+      })
+    }).catch(err => res.tools.setJson(1, '查询错误', err))
   }
 }
 
